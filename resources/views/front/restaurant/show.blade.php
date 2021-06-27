f1cb463ce5a6cffd7fb7ace64b1c14dec9026bcc
Mohamed Samy <mohamedsabaawy@Hotmail.com>
2021-06-26 20:39:08 -0700

    test

--- resources/views/front/restaurant/show.blade.php
+++ resources/views/front/restaurant/show.blade.php
@@ -353,7 +353,7 @@
 <!--start About Us-->
 <div class="about-us">
     <div class="text">
-        <h2>Discover</h2>
+        <h2>{{$restaurant->name}}</h2>
         <h3>Our Story</h3>
         <p>We are a restaurant and coffee roastery since 1995 located on a busy corner site in Sharm El-Sheikh. With glazed frontage on two sides of the building, overlooking midtown.</p>
     </div>
